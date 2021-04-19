bb8bf47a6d1f87701fa9b7785f91dc7f8aea4d00
Michael <mnath10@stuy.edu>
2021-04-19 19:51:09 -0400

    added most of stuff for research section

--- src/App.js
+++ src/App.js
@@ -1,30 +1,84 @@
-import { Tabs, Tab, makeStyles } from "@material-ui/core";
+import { Tabs, Tab, makeStyles, IconButton } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Projects from "./Projects";
+import ProjectTab from "./ProjectTab.js";
+import { useState } from "react";
 import "./App.css";
 
 const useStyles = makeStyles({
-	header: {
+ 	header: {
 		color: "#FBFCD4",
 	},
 	tabs: {
-		backgroundColor: "#151515",
+		backgroundColor: "#0A0A0A",
 	},
 	tab: {
 		color: "#FFDD03",
 	},
 });
 
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		"aria-controls": `simple-tabpanel-${index}`,
+	};
+}
+
 function App() {
 	const classes = useStyles();
+	const [value, setValue] = useState(0);
+	const tabs = ["RESEARCH", "ENGINEERING", "LEISURE"];
+	const [projects, setProjects] = useState(Projects["content"][value]);
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+		setProjects(Projects["content"][newValue]);
+	};
 	return (
 		<div className="App">
 			<h1 className={classes.header}>Hi. I'm Michael Nath</h1>
 			{/* Links to Stuff */}
-			<h2 className={classes.header}>Github Linkedin</h2>
-			<Tabs className={classes.tabs} textColor="primary" centered={true}>
-				<Tab className={classes.tab} label="RESEARCH" />
-				<Tab className={classes.tab} label="ENGINEERNIG" />
-				<Tab className={classes.tab} label="LEISURE" />
+			<h2 className={classes.header}>
+				<a href="https://github.com/Michael-Nath">
+					<IconButton>
+						<GitHubIcon style={{ color: "#FBFCD4" }} />
+					</IconButton>{" "}
+				</a>
+				<a href="https://www.linkedin.com/in/michael-nath/">
+					<IconButton>
+						<LinkedInIcon style={{ color: "#FBFCD4" }} />
+					</IconButton>
+				</a>
+			</h2>
+			<Tabs
+				onChange={handleChange}
+				value={value}
+				className={classes.tabs}
+				textColor="primary"
+				centered={true}
+			>
+				{tabs.map((item, i) => {
+					return (
+						<Tab
+							key={i}
+							className={classes.tab}
+							label={item}
+							{...a11yProps(i)}
+						/>
+					);
+				})}
 			</Tabs>
+			{projects.map((item, i) => {
+				return (
+					<ProjectTab
+						name={item.name}
+						description={item.description}
+						tools={item.tools}
+						docs={item.docs != undefined ? item.docs : []}
+						key={i}
+					></ProjectTab>
+				);
+			})}
 		</div>
 	);
 }
---
+++ src/ProjectTab.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {
+	CardContent,
+	Card,
+	CardActions,
+	CardHeader,
+	makeStyles,
+	Button,
+	Collapse,
+	IconButton,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+} from "@material-ui/core";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import BuildIcon from "@material-ui/icons/Build";
+import AdjustIcon from "@material-ui/icons/Adjust";
+
+const useStyles = makeStyles({
+	card: {
+		backgroundColor: "#151515",
+		marginBottom: "5vh",
+	},
+	header: {
+		color: "#FBFCD4",
+	},
+	description: {
+		color: "white",
+	},
+	button: {
+		color: "#FFDD03",
+	},
+});
+
+function ProjectTab({ name, description, tools, docs }) {
+	const classes = useStyles();
+	const [expanded, setExpanded] = useState(false);
+	return (
+		<Card className={classes.card} raised={true}>
+			<CardHeader className={classes.header} title={name} />
+			<CardContent>
+				<h3 className={classes.description}>{description}</h3>
+				<Collapse in={expanded} unmountOnExit>
+					<CardContent>
+						<List className={classes.description}>
+							<ListItem component="h2">
+								Tools Learned
+							</ListItem>
+							{tools.map((tool, i) => {
+								return (
+									<ListItem component="h4" key={i}>
+										<ListItemIcon>
+											<AdjustIcon className={classes.description} />
+										</ListItemIcon>
+										<ListItemText primary={tool} />
+									</ListItem>
+								);
+							})}
+						</List>
+					</CardContent>
+				</Collapse>
+				<IconButton
+					className={classes.description}
+					onClick={() => setExpanded(!expanded)}
+				>
+					{expanded ? <ExpandLessIcon/> : <BuildIcon />}
+				</IconButton>
+			</CardContent>
+			<CardActions>
+				{docs.map((doc, i) => {
+					return (
+						<Button href={doc.link} className={classes.button} key={i}>
+							{doc.name}
+						</Button>
+					);
+				})}
+			</CardActions>
+		</Card>
+	);
+}
+export default ProjectTab;
